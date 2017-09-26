098f4abe76e1bff3f92c5dee3431d96e9e43713d
Vincent Lopes <vlopes@openstudio.fr>
2017-09-26 15:12:39 +0200

    Add input to update all zone for a module with one input + fix module.xml

--- I18n/backOffice/default/fr_FR.php
+++ I18n/backOffice/default/fr_FR.php
@@ -1,9 +1,10 @@
 <?php
+
 return array(
-    "Don't use taxes" => "Ne pas utiliser les taxes",
-    "Free delivery" => "Livraison gratuit",
-    "If the customer cart price is above the following price, the delivery is free. Should we take into account the cart price with or without taxes ?" => "Si le prix du panier du client est supérieur au prix spécifié ci-dessous, la livraison sera gratuite. Doit-on prendre en compte les taxes dans le prix du panier pour effectuer la comparaison ?",
-    "Invalid value : %value" => "Valeur incorrecte : %value",
-    "Price parameters" => "Paramètres de prix",
-    "Use taxes" => "Utiliser les taxes"
+    'Don\'t use taxes' => 'Ne pas utiliser les taxes',
+    'Free delivery' => 'Livraison gratuit',
+    'If the customer cart price is above the following price, the delivery is free. Should we take into account the cart price with or without taxes ?' => 'Si le prix du panier du client est supérieur au prix spécifié ci-dessous, la livraison sera gratuite. Doit-on prendre en compte les taxes dans le prix du panier pour effectuer la comparaison ?',
+    'Price parameters' => 'Paramètres de prix',
+    'Update all zones for one module' => 'Modification de toutes les zones d\'un module',
+    'Use taxes' => 'Utiliser les taxes',
 );
